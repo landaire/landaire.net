10339326b1325fc64d7f88805b22a903c4f29331
Lander Brandt <landerbrandt@gmail.com>
2014-12-01 12:39:59 -0800

    Add Godeps

---
+++ Godeps/_workspace/src/github.com/landaire/go-taglib/README.md
@@ -0,0 +1,29 @@
+go-taglib
+=========
+
+Go wrapper for [taglib](http://taglib.github.com/)
+[![Build Status](https://travis-ci.org/landaire/go-taglib.png)](https://travis-ci.org/landaire/go-taglib)
+
+Dependencies
+------------
+
+You must have the static [taglib](http://taglib.github.com/) libraries installed
+in order to compile go-taglib.
+
+### OSX:
+
+    brew install taglib
+
+### Ubuntu:
+
+    sudo apt-get install libtagc0-dev
+
+Install
+-------
+
+    go get github.com/landaire/go-taglib
+
+Documentation
+-------------
+
+Checkout the documentation at http://go.pkgdoc.org/github.com/landaire/go-taglib
---
+++ Godeps/_workspace/src/github.com/landaire/go-taglib/taglib_test.go
@@ -0,0 +1,127 @@
+package taglib
+
+import (
+	"testing"
+	"time"
+)
+
+func TestReadNothing(t *testing.T) {
+	file, err := Read("doesnotexist.mp3")
+
+	if file != nil {
+		t.Fatal("Returned non nil file struct.")
+	}
+
+	if err == nil {
+		t.Fatal("Returned nil err.")
+	}
+
+	if err != ErrInvalid {
+		t.Fatal("Didn't return ErrInvalid")
+	}
+}
+
+func TestReadDirectory(t *testing.T) {
+	file, err := Read("/")
+
+	if file != nil {
+		t.Fatal("Returned non nil file struct.")
+	}
+
+	if err == nil {
+		t.Fatal("Returned nil err.")
+	}
+
+	if err != ErrInvalid {
+		t.Fatal("Didn't return ErrInvalid")
+	}
+}
+
+func TestTagLib(t *testing.T) {
+	file, err := Read("test.mp3")
+
+	if err != nil {
+		panic(err)
+		t.Fatalf("Read returned error: %s", err)
+	}
+
+	defer file.Close()
+
+	// Test the Tags
+	if title := file.Title(); title != "The Title" {
+		t.Errorf("Got wrong title: %s", title)
+	}
+
+	if artist := file.Artist(); artist != "The Artist" {
+		t.Errorf("Got wrong artist: %s", artist)
+	}
+
+	if album := file.Album(); album != "The Album" {
+		t.Errorf("Got wrong album: %s", album)
+	}
+
+	if comment := file.Comment(); comment != "A Comment" {
+		t.Errorf("Got wrong comment: %s", comment)
+	}
+
+	if genre := file.Genre(); genre != "Booty Bass" {
+		t.Errorf("Got wrong genre: %s", genre)
+	}
+
+	if year := file.Year(); year != 1942 {
+		t.Errorf("Got wrong year: %d", year)
+	}
+
+	if track := file.Track(); track != 42 {
+		t.Errorf("Got wrong track: %d", track)
+	}
+
+	// Test the properties
+	if length := file.Length(); length != 42*time.Second {
+		t.Errorf("Got wrong length: %s", length)
+	}
+
+	if bitrate := file.Bitrate(); bitrate != 128 {
+		t.Errorf("Got wrong bitrate: %d", bitrate)
+	}
+
+	if samplerate := file.Samplerate(); samplerate != 44100 {
+		t.Errorf("Got wrong samplerate: %d", samplerate)
+	}
+
+	if channels := file.Channels(); channels != 2 {
+		t.Errorf("Got wrong channels: %d", channels)
+	}
+
+	// Test writing
+	file.SetTitle("TEST")
+	file.Save()
+
+	if title := file.Title(); title != "TEST" {
+		t.Errorf("Got the wrong title: %s (expecting %s)", title, "TEST")
+	}
+
+	// Test writing the title name back
+	file.SetTitle("The Title")
+	file.Save()
+
+	if title := file.Title(); title != "The Title" {
+		t.Errorf("Got the wrong title when reverting change: %s (expecting %s", title, "The Title")
+	}
+
+	// Test setting the artist
+	file.SetArtist("Test Artist")
+	file.Save()
+
+	if artist := file.Artist(); artist != "Test Artist" {
+		t.Errorf("Got the wrong artist: %s (expecting %s)", artist, "Test Artist")
+	}
+
+	// Test writing back the original artist name
+	file.SetArtist("The Artist")
+	file.Save()
+
+	if artist := file.Artist(); artist != "The Artist" {
+		t.Errorf("Got the wrong artist when reverting change: %s (expecting %s)", artist, "The Artist")
+	}
+}
